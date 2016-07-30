CyanogenMod/android_packages_services_Telecomm

Telecomm : Allow multiple default dialers
Change-Id: Ied3fe36cf5249b9ab5a9f4bdd36f2ba6e76c538e

src/com/android/server/telecom/InCallController.java
@@ -154,9 +154,7 @@ public final class InCallController extends CallsManagerListenerBase {
         mContext = context;
         Resources resources = mContext.getResources();
 
-        mInCallComponentName = new ComponentName(
-                resources.getString(R.string.ui_default_package),
-                resources.getString(R.string.incall_default_class));
+        mInCallComponentName = TelephonyUtil.getInCallComponentName(context);
     }
 
     @Override

LiquidNougat/packages_services_Telecomm

src/com/android/server/telecom/InCallController.java
@@ -162,9 +162,7 @@ public final class InCallController extends CallsManagerListenerBase {
         mCallsManager = callsManager;
         Resources resources = mContext.getResources();
 
-        mSystemInCallComponentName = new ComponentName(
-                resources.getString(R.string.ui_default_package),
-                resources.getString(R.string.incall_default_class));
+        mSystemInCallComponentName = TelephonyUtil.getInCallComponentName(context);
     }
 
     @Override